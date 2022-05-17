57cae2f1288eedb3a2cc1ea63db745b268e5ae0e
DanielSlusarczyk <daniel00150015@gmail.com>
2022-05-17 22:10:04 +0200

    Dodanie przykładów do BLEU

--- funkcje/ROUGE_functions.py
+++ funkcje/ROUGE_functions.py
@@ -59,5 +59,74 @@ def calculateRecall(ngram, reference, candidate):
         sumOfGram = sumOfGram + gramOccurrances
         print(str(gramOccurrances))
 
-    recall = sumOfGram/len(unique_reference)
+    recall = sumOfGram/nmbOfGrams
     return recall
+
+# Definiuje liczbę rozpatrywanych n-gramów zależnie od przypisanych wag
+def getNumberOfnGram(weights):
+    n = len(weights)
+    result = 0
+    for i in range(1, n):
+        if( weights[i - 1] == 0 ):
+            break
+        else:
+            result = i
+    return result + 1
+
+# Obliczanie miary Precision
+def calculatePrecision(ngram, reference, candidate):
+    # Zmienne pomocnicze
+    unique_reference = makeUniqueList(reference)
+    tabs = '\t\t\t\t'
+    sumOfGram = 0
+
+    # Liczba n-gramów:
+    if(ngram > 1):
+        unique_reference = reference
+    nmbOfGrams = len(unique_reference) - ngram + 1
+    counter = 1
+
+    # Pętla po liczbie n-gramów zależna od rozpatrywanego n-gramu
+    for idOfPattern in range(0, nmbOfGrams):
+
+        gram = unique_reference[idOfPattern]
+
+        # Pętla po liczbie słów do dodania aby otrzymać n-gram
+        for idToAdd in range(1, ngram):
+            gram = gram + ' ' + unique_reference[idOfPattern + idToAdd]
+
+        #Wypisanie początku wiersza tabeli
+        print("{0:<4}".format(str(counter)+ ")"), end='')
+        print("{0:<20}".format(gram), end='')
+        print(tabs, end='')
+
+        #Zmienne dla konkretnego n-gramu:
+        counter = counter + 1
+        candidateWithSpace = ' '.join(map(str, candidate))
+
+        # Sprawdzenie wystąpienia konkretnego n-gramu w referencji
+        gramOccurrances = getNumberOfOccurance(gram, candidateWithSpace)
+        sumOfGram = sumOfGram + gramOccurrances
+        print(str(gramOccurrances))
+
+    precision = sumOfGram/(len(makeUniqueList(candidate)) - ngram + 1)
+    return precision
+
+# Obliczanie miary F1 Score
+def calculateF1(recall, precision):
+    return 2 * (recall * precision)/(precision + recall)
+
+# Zwracanie długości najdłuższego wspólnego podłańcucha
+def longestCommonSubsequence(text1 , text2):
+    m = len(text1)
+    n = len(text2)
+    matrix = [[0]*(n+1) for i in range(m+1)] 
+    for i in range(m+1):
+        for j in range(n+1):
+            if i==0 or j==0:
+                matrix[i][j] = 0
+            elif text1[i-1] == text2[j-1]:
+                matrix[i][j] = 1 + matrix[i-1][j-1]
+            else:
+                matrix[i][j] = max(matrix[i-1][j] , matrix[i][j-1])
+    return matrix[-1][-1]
